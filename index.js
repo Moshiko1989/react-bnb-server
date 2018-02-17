79341ca9dc74226e6e55538b2e83467501284746
Moshiko_Levi <moshikolevi1989@gmail.com>
2018-02-17 21:32:12 +0200

    5000

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const cl = console.log;
 
-// Importing Express.
+// Importing Extensions.
 const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // Set up Express app.
 app = express();
+
+const dev = app.get('env') !== 'production';
+
 // Conect to mongo db.
 const options = {
     useMongoClient: true,
@@ -16,14 +19,17 @@ const options = {
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
     bufferMaxEntries: 0
-  };
+};
 mongoose.connect('mongodb://moshiko:9301306moshiko@ds125288.mlab.com:25288/react-bnb', options);
 mongoose.Promise = global.Promise;
 
 /////////////////////// Middlewares ///////////////////////////////
 
+
+
 // Not sure what it does yet, related to serving front end files.
 // app.use(express.static('public'));
+app.use(express.static('static'));
 
 // Using Cors to overcome cors in dev mode.
 app.use(cors());
@@ -41,11 +47,12 @@ app.use(function (err, req, res, next) {
     res.status(422).send({ error: err.message })
 });
 
+
 //////////////////////// Listen /////////////////////////////////
 
 // Listen to requests.
-app.listen(process.env.port || 4000, function () {
+app.listen(process.env.PORT || 5000, function () {
     cl('******************************');
-    cl('**  listening to port 4000  **');
+    cl(`**  listening to port ${process.env.port || 5000}  **`);
     cl('******************************');
 })
